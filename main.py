fec1906dd259b93087e2c25be063af53529928f4
hiroyuki01233 <hriohrio311@gmail.com>
2022-11-29 01:58:41 +0800

    Zenn tutorial

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-import pandas as pd
-data = pd.read_csv('consumerPrices_tree.csv')
-data.head()
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+
+iris = datasets.load_iris()
+data_train, data_test, target_train, target_test = train_test_split(iris.data, iris.target)
+clf = MLPClassifier()
+clf.fit(data_train, target_train)
+print(clf.score(data_test, target_test))
